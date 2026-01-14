39cf6277853f30a3eb7ab062d837cbcf7fafef26
Cameron Abrams <cfa22@drexel.edu>
2026-01-14 17:55:43 -0500

    updates

--- src/sandlermisc/__init__.py
+++ src/sandlermisc/__init__.py
@@ -1,4 +1,5 @@
 from .gas_constant import GasConstant
 from .thermals import DeltaH_IG, DeltaS_IG
+from .statereporter import StateReporter
 
-__all__ = [ 'GasConstant', 'DeltaH_IG', 'DeltaS_IG' ]
+__all__ = [ 'GasConstant', 'DeltaH_IG', 'DeltaS_IG', 'StateReporter' ]
